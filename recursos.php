db43ad6ce8afe60f022f41d71bc980f564ddff94
Axel Céspedes <axelt691@gmail.com>
2025-08-04 02:48:42 -0600

    Agregue preguntas frecuentes y guías en recursos.php

--- recursos.php
+++ recursos.php
@@ -1,25 +1,56 @@
-<?php
-include 'header.php';
-?>
-<main class="recurso-box">
-  <h2>📚 Centro de Recursos</h2>
-  <h3>🩺 Guías</h3>
-  <ul>
-    <li><a href="docs/guia_rcp.pdf" target="_blank">RCP</a></li>
-    <li><a href="docs/manual_heridas.pdf" target="_blank">Heridas</a></li>
-  </ul>
+<?php include 'header.php'; ?>
+<main class="contenedor-recursos">
+  <title>Centro de recursos</title>
+  <section class="card guia">
+    <h3>Guías De Primeros Auxilios</h3>
+    <ul>
+      <li>
+        Manual de primeros auxilios
+        <a href="https://www.unirioja.es/servicios/sprl/pdf/manual_primeros_auxilios.pdf" target="_blank">[PDF]</a>
+      </li>
+      <li>
+        Guía para hemorragias
+        <a href="https://www.gob.mx/cms/uploads/attachment/file/732472/HeridasHemorragias_compressed.pdf" target="_blank">[PDF]</a>
+      </li>
+      <li>
+        Guía para quemaduras
+        <a href="https://www.carm.es/web/descarga?ARCHIVO=FD7.pdf&ALIAS=ARCH&IDCONTENIDO=21533&RASTRO=c911$m6407,4580,5957">[PDF]</a>
+      </li>
+      <li>
+        Asistencia en fracturas 
+        <a href="https://www.hospitalyali.gov.co/wp-content/uploads/2015/07/GUIA-PARA-EL-MANEJO-DE-FRACTURAS.pdf">[PDF]</a>
+      </li>
+      <li>
+        Obstrucción respiratoria
+        <a href="https://www.cso.go.cr/legislacion/notas_tecnicas_preventivas_insht/NTP%20467%20-%20Obstruccion%20de%20las%20vias%20respiratorias%20primeros%20auxilios.pdf">[PDF]</a>
+      </li>
+      <li>
+        Actuar ante ahogamiento
+        <a href="https://www.aeped.es/sites/default/files/ahogamiento_csvaep_definitivo_12-06-2024.pdf">[PDF]</a>
+      </li>
+    </ul>
+</section>
 
-  <h3>📞 Emergencias</h3>
+ <!-- Numeros de emergencia -->
+<section class="card emergencias">
+  <h3>Números De Emergencia</h3>
   <ul>
-    <li>911 – Nacional</li>
-    <li>112 – Cruz Roja</li>
-    <li>118 – Bomberos</li>
+    <li>911 - Nacional</li>
+    <li>128 - Cruz Roja</li>
+    <li>118 - Bomberos y Rescate</li>
+    <li>127 - Policia Rural</li>
   </ul>
+</section>
 
-  <h3>❓ FAQ</h3>
+<section class="card preguntas">
+  <h3>Preguntas Frecuentes</h3>
   <ul>
-    <li><strong>¿Quién puede voluntariar?</strong> Solo certificados.</li>
-    <li><strong>¿Cómo verifican?</strong> Admin revisa docs.</li>
+    <li><strong>¿Quién puede participar?</strong> Solo certificados.</li>
+    <li><strong>¿Cómo me puedo verificar?</strong> Con documentos subidos.</li>
+    <li><strong>¿Dónde se atiende?</strong> En comunidades registradas.</li>
+    <li><strong>¿Qué debería hacer si veo a alguien desmayado en la calle?</strong> Primero verificar si la persona habla o responde y llamar inmediatamente al 911.</li>
+    <li><strong>¿Es seguro aplicar RCP si no estoy 100% seguro de lo que hago?</strong> Aunque no tengas capacitación lo mejor es primero llamar al 911 y seguir las indicaciones.</li>
   </ul>
+</section>
 </main>
 <?php include 'footer.php'; ?>
